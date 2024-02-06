cccc967e9cb229e6cb3397e887a71779be9a364d
Trysten M <nick.tristen22@gmail.com>
2024-02-07 00:38:13 +0200

    add(components/users): add user phone component

---
+++ src/components/users/fields/UserPhone.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  value: {
+    type: String,
+  },
+  label: {
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="user-phone">
+    <a :href="`tel:${value}`">
+      {{ label || value }}
+    </a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
