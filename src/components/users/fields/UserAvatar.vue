cd11ec549dcfea9252889e681f3c7cc78273bcd5
Trysten M <nick.tristen22@gmail.com>
2024-02-07 00:49:14 +0200

    update(components/users): update user avatar

--- src/components/users/fields/UserAvatar.vue
+++ src/components/users/fields/UserAvatar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {computed, reactive} from "vue";
+import VImage from "@/components/UI/VImage.vue";
 
 const props = defineProps({
   src: {
@@ -15,55 +15,15 @@ const props = defineProps({
     type: Number
   }
 })
-
-const state = reactive({
-  isLoad: false,
-  isError: false
-})
-
-function onLoad() {
-  state.isError = false
-  state.isLoad = true
-}
-
-function onError() {
-  state.isError = true
-  state.isLoad = false
-}
-
-const showImage = computed(() => {
-  return state.isLoad && !state.isError && props.src
-})
-
-const showInitial = computed(() => {
-  return !state.isLoad || state.isError || !props.src
-})
-
-const style = computed(() => [
-  props.fontSize ? `font-size: ${props.fontSize}px` : ''
-].join('; '))
 </script>
 
 <template>
   <div class="user-list-avatar">
-    <div class="image-wrapper">
-      <transition name="fade" appear :duration="2000">
-        <img
-            v-show="showImage"
-            :src="src"
-            :alt="alt"
-            @load="onLoad"
-            @error="onError"
-        >
-      </transition>
-      <div
-          class="initial"
-          :class="showInitial && 'show'"
-          :style="style"
-      >
-        {{ initial }}
-      </div>
-    </div>
+    <VImage
+        :src="src"
+        :alt="alt"
+        :initial="initial"
+    />
   </div>
 </template>
 
@@ -74,45 +34,5 @@ const style = computed(() => [
   flex: 1;
   width: 100%;
   min-width: 40px;
-}
-
-.image-wrapper {
-  position: relative;
-  width: 0;
-  height: 0;
-  padding-left: 100%;
-  padding-top: 100%;
-  border-radius: 50%;
-  overflow: hidden;
-  transition: .2s ease;
-}
-
-.initial,
-img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  transition: .5s ease;
-}
-
-img {
-  display: block;
-  object-fit: cover;
-}
-
-.initial {
-  color: $COLOR_MAIN_LIGHT;
-  background: rgb(102, 151, 212);
-  flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  opacity: 0;
-
-  &.show {
-    opacity: 1;
-  }
 }
 </style>
