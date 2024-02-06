32f9431fb142fa58ebac569fec042318bd5fb4b9
Trysten M <nick.tristen22@gmail.com>
2024-02-07 00:41:18 +0200

    update(helper/icons): add new icons

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -3,4 +3,6 @@ export default {
     ADD: iconPath + 'add-circle-svgrepo-com.svg',
     DELETE: iconPath + 'cross-square-svgrepo-com.svg',
     EDIT: iconPath + 'edit-svgrepo-com.svg',
+    CLOSE: iconPath + 'close-circle-svgrepo-com.svg',
+    UPLOAD: iconPath + 'upload-minimalistic-svgrepo-com.svg',
 }
