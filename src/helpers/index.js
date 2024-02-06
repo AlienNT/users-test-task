ead3722c1776af753cf4cf34b72d56d0b590df3c
Trysten M <nick.tristen22@gmail.com>
2024-02-07 00:53:20 +0200

    add(helpers): add getInitials & fileToBase methods

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,9 +3,22 @@ export function debounce(func, delay = 100) {
 
     return () => {
         clearTimeout(timer)
-        timer = setTimeout(() => {
-            console.log('func')
-            return func()
-        }, delay)
+        timer = setTimeout(() => func(), delay)
     }
+}
+
+export function getInitials(...fields) {
+    return fields?.length ?
+        fields.map(string => string?.length ? string[0].toUpperCase() : '') :
+        null
+}
+
+export function fileToBase(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+    });
 }
