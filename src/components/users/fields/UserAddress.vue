68d6140955e55edb3f17b38a5b9d8b9073e3b0b3
Trysten M <nick.tristen22@gmail.com>
2024-02-07 00:37:51 +0200

    add(components/users): add user address component

---
+++ src/components/users/fields/UserAddress.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  value: {
+    type: String
+  }
+})
+</script>
+
+<template>
+<div class="user-address">
+  {{value}}
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
