b035fba3dee69411920554916d72605e8b0bdba0
Trysten M <nick.tristen22@gmail.com>
2024-02-07 00:47:07 +0200

    update(components/app): add response status indicator; set viewport height prop

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import UsersPage from "@/pages/UsersPage.vue";
+import ResponseStatusIndicator from "@/components/UI/ResponseStatusIndicator.vue";
+
+import {onMounted} from "vue";
+
+onMounted(() => setViewportHeight())
+
+window.onresize = () => setViewportHeight()
+
+function setViewportHeight() {
+  document.documentElement.style.setProperty('--vh', window.innerHeight + 'px')
+}
 </script>
 
 <template>
   <UsersPage/>
+  <ResponseStatusIndicator/>
 </template>
-
-<style scoped>
-</style>
