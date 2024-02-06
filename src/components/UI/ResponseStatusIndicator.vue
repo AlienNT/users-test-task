14c0a9e21b61a7c664a7c065fd5fbd42251a8e71
Trysten M <nick.tristen22@gmail.com>
2024-02-07 00:37:28 +0200

    add(components/UI): add response status indicator

---
+++ src/components/UI/ResponseStatusIndicator.vue
@@ -0,0 +1,42 @@
+<script setup>
+import {useApiRequest} from "@/composables/api/useApiRequest.js";
+import {computed, watch} from "vue";
+
+const {loading, isSuccess, isError} = useApiRequest()
+
+const className = computed(() => {
+  if (isSuccess.value) return 'success'
+  if (isError.value) return 'error'
+  if (loading.value) return 'loading'
+
+  return null
+})
+</script>
+
+<template>
+  <div
+      :class="className"
+      class="response-indicator"
+  />
+</template>
+
+<style scoped lang="scss">
+.response-indicator {
+  width: 100%;
+  height: 4px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 8;
+  transition: .5s ease;
+}
+.loading {
+  background: #ffbc41;
+}
+.success {
+  background: #00d500;
+}
+.error {
+  background: #ff4f4f;
+}
+</style>
